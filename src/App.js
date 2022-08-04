f8fc7c10477248c2f40344a612445ff91a5bede6
miggea <miggea@localhost.localdomain>
2022-08-04 04:24:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          File 002
+                  </a>
       </header>
     </div>
   );
