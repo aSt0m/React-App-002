fd95d3a648ecb08a1f57b8703d84fb5e1e7df2ad
miggea <miggea@localhost.localdomain>
2022-08-04 16:36:46 -0500

    Updates

---
+++ precache-manifest.ee34d6ce0b2f34d05a02e8a0de95e64a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8dfa1d688cb56b34493",
+    "url": "/React-App-002/static/css/main.f4dcb52e.chunk.css"
+  },
+  {
+    "revision": "d8dfa1d688cb56b34493",
+    "url": "/React-App-002/static/js/main.d8dfa1d6.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/React-App-002/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "c8817e6860b6f9987083",
+    "url": "/React-App-002/static/js/2.c8817e68.chunk.js"
+  },
+  {
+    "revision": "a2be9e9f6def4a32c222",
+    "url": "/React-App-002/static/js/runtime~main.a2be9e9f.js"
+  },
+  {
+    "revision": "da53869a7abea27c6e6a65519260716c",
+    "url": "/React-App-002/static/media/webb-images-full-width.jpg.thumb.1160.1160.da53869a.jpg"
+  },
+  {
+    "revision": "dc5d54f72a5215cd439eace4fab3b130",
+    "url": "/React-App-002/index.html"
+  }
+];
