8d8860fd439f8e9e22e5276856ee54c0fb153d0b
miggea <miggea@localhost.localdomain>
2022-08-04 16:29:32 -0500

    ffff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{ Fragment } from 'react';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          File 002
-                  </a>
-      </header>
+    <Fragment>
+
+    <div >
+      <Header />
+      
     </div>
+    </Fragment>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React, { Fragment,useEffect } from 'react';
+import gsap from 'gsap';
+
+const Header = () => {
+    useEffect(() => {
+        // default(x:-100)
+        gsap.fromTo(".title1",{x:-100,opacity:0,duration:4},{opacity:1,x:0, duration:4, ease:"none" })
+        gsap.fromTo(".title2",{x: 100,opacity:0},{opacity:1,x:0, duration:2, ease:"none" })
+      
+    
+    }, [])
+    
+    return ( 
+        <Fragment>
+            <div className='Header' > 
+                <div className='container__header'>
+
+                <div className='Header__left title1'>
+                    <h1>Im title left</h1>
+                </div>
+                <div className='Header__right title2'>
+                    <p>Im paragraph right Im paragraph right Im paragraph right </p>
+                </div>
+                </div>
+                
+            </div>
+        </Fragment>
+     );
+}
+ 
+export default Header;
